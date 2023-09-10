34035ab9a5a4c400e71321ebb3d9ded670ec9dea
Israel1710 <143962633+Israel1710@users.noreply.github.com>
2023-09-10 17:25:49 -0600

    Initial commit

---
+++ src/main/kotlin/HELLOUPIICSA.kt
@@ -0,0 +1,30 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main() {
+    print("HelloUPIICSA")
+
+    var number1 = 11
+    println(number1)
+    number1 + 20
+
+    val number2 = 8
+    println(number2)
+
+    val implicitString = "1er String"
+    val implicitInt = 15
+    println(implicitString.javaClass.kotlin)
+    // class.kotlin.string
+    // val explicitInt: Int = "2do String"
+
+    val oneMillion = 1000000
+    println(oneMillion)
+
+    val Char = 'A'
+    // val BoolTrue = True
+    // val BoolFalse = False
+    // val 1erString: String = "2do String"
+
+    val edad = 19
+    println("MI edad es $edad")
+    println("Mi edad dentro de 2 años sera ${edad+2} años")
+}
