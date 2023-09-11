7b0443e5c322b579b7ba59c1f67a167396473a55
Israel1710 <143962633+Israel1710@users.noreply.github.com>
2023-09-10 18:34:24 -0600

    2da Práctica

---
+++ src/main/kotlin/VariablesYTiposDeDatos.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var decimal = 8.5
+    println(decimal)
+
+    var float = decimal.toFloat()
+    println(float)
+
+    val c = 3.14159265359
+    var resultado = c*(decimal*2)
+
+    println("El perímetro del círculo es: $resultado")
+
+}
