80d3a486922d6e0dab8660de7409cd9857d8ff68
Israel1710 <143962633+Israel1710@users.noreply.github.com>
2023-09-28 01:44:47 -0600

    Add files via upload
    Práctica 1

---
+++ Variables_y_tipos_de_datos/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+const val PI = 3.15416
+fun main() {
+    val numDecimal = 8.5f
+    //println(numDecimal.class.kotlin)
+
+    val perimetro = 2*PI*numDecimal
+
+    println("El perímetro del círculo es: $perimetro")
+}
